fa80f8e823c353c192ce6bf9020b26d2c5337fc5
Jenan Alfahham <jenan_fh95@hotmail.com>
2024-11-19 19:28:53 +0300

    BOQ Doctype

---
+++ furniture/furniture/doctype/boq/boq.js
@@ -0,0 +1,260 @@
+// Copyright (c) 2024, Jenan Alfahham and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('BOQ', {
+	setup: function(frm){
+		frm.set_query("party_type", function() {
+			return{
+				"filters": {
+					"name": ["in", ["Customer", "Lead", "Prospect"]],
+				}
+			}
+		});
+		frm.set_query("boq_template", function() {
+			return{
+				"filters": {
+					"item_code": frm.doc.item,
+				}
+			}
+		});
+	},
+	onload: function(frm){
+		frm.trigger('setup_queries');
+	},
+	boq_template: function(frm){
+		if (frm.doc.item && frm.doc.boq_template){
+			frappe.call({
+				"method": "furniture.furniture.doctype.boq.boq.set_boq_template",
+				"args": {
+					"boq_template": frm.doc.boq_template,
+					"doc": frm.doc
+				},
+				callback: function(r){
+					if (r.message) {
+						frm.clear_table("material_costs");
+						frm.clear_table("labor_costs");
+						frm.clear_table("contractors_table");
+						frm.clear_table("expenses_table");
+						let keys = Object.keys(r.message);
+
+						for (const table of keys){
+							for (const row of r.message[table]){
+								let c = frm.add_child(table);
+								c.cost = row.cost;
+								c.item = row.item;
+								c.qty = row.qty;
+								c.total_cost = row.total_cost;
+
+								if (table == "material_costs") c.depreciasion_percentage = row.depreciasion_percentage;
+
+								else c.uom = row.uom
+							}
+							
+							frm.refresh_field(table)
+
+						}
+
+					}
+				}
+			})
+		}
+	},
+	party_type: function(frm) {
+		frm.trigger('setup_party_type');
+
+		frm.set_value("party_name", "");
+	},
+	setup_party_type: function(frm) {
+		frm.trigger('setup_queries');
+		frm.trigger("set_dynamic_field_label");
+	},
+	set_dynamic_field_label: function(frm){
+		if (frm.doc.party_type) {
+			frm.set_df_property("party_name", "label", frm.doc.party_type);
+		}
+	},
+	setup_queries: function(frm){
+		if (frm.doc.party_type == "Lead") {
+			frm.set_query('party_name', erpnext.queries['lead']);
+		}
+		else if (frm.doc.party_type == "Customer") {
+			frm.set_query('party_name', erpnext.queries['customer']);
+		} else if (frm.doc.party_type == "Prospect") {
+			frm.set_query('party_name', function() {
+				return {
+					filters: {
+						"company": me.frm.doc.company
+					}
+				};
+			});
+		}
+	}
+});
+
+frappe.ui.form.on('Material costs', {
+	material_costs_add: function(frm, cdt, cdn) {
+		let row = locals[cdt][cdn];
+		row.qty = 1;
+		row.depreciasion_percentage = 0;
+		row.cost = 0;
+		frm.refresh_field("material_costs")
+	},
+	item: function(frm, cdt, cdn){
+		let row = locals[cdt][cdn];
+		frappe.db.get_value("Item", {"item_code": row.item}, "stock_uom", (r) => {
+			if (r) {
+				frappe.model.set_value(cdt, cdn, "uom", r.stock_uom);
+				frappe.model.set_value(cdt, cdn, "conversion_factor", 1)
+			}
+		})
+	},
+	qty: function(frm, cdt, cdn){
+		let row = locals[cdt][cdn];
+		frappe.model.set_value(cdt, cdn, "total_cost", row.qty * row.cost);
+	},
+	last_purchase_rate: function(frm, cdt, cdn){
+		let row = locals[cdt][cdn];
+		frappe.model.set_value(cdt, cdn, "cost", row.last_purchase_rate);
+	},
+	cost: function(frm, cdt, cdn){
+		let row = locals[cdt][cdn];
+		frappe.model.set_value(cdt, cdn, "total_cost", row.qty * row.cost);
+	},
+	uom: function(frm, cdt, cdn){
+		let row = locals[cdt][cdn];
+		set_conversion_factor(frm, row);
+	},
+	conversion_factor: function(frm, cdt, cdn){
+		let row = locals[cdt][cdn];
+		frappe.model.set_value(cdt, cdn, "cost", row.conversion_factor * row.last_purchase_rate);
+	}
+});
+
+frappe.ui.form.on('Labor costs', {
+	labor_costs_add: function(frm, cdt, cdn) {
+		let row = locals[cdt][cdn];
+		row.qty = 1;
+		row.cost = 0;
+		frm.refresh_field("labor_costs")
+	},
+	qty: function(frm, cdt, cdn){
+		let row = locals[cdt][cdn];
+		frappe.model.set_value(cdt, cdn, "total_cost", row.qty * row.cost);
+	},
+	cost: function(frm, cdt, cdn){
+		let row = locals[cdt][cdn];
+		frappe.model.set_value(cdt, cdn, "total_cost", row.qty * row.cost);
+	},
+	item: function(frm, cdt, cdn){
+		let row = locals[cdt][cdn];
+		frappe.db.get_value("Item", {"item_code": row.item}, "stock_uom", (r) => {
+			if (r) {
+				frappe.model.set_value(cdt, cdn, "uom", r.stock_uom);
+				frappe.model.set_value(cdt, cdn, "conversion_factor", 1);
+			}
+		})
+	},
+	uom: function(frm, cdt, cdn){
+		let row = locals[cdt][cdn];
+		set_conversion_factor(frm, row);
+	},
+	last_purchase_rate: function(frm, cdt, cdn){
+		let row = locals[cdt][cdn];
+		frappe.model.set_value(cdt, cdn, "cost", row.last_purchase_rate);
+	},
+	conversion_factor: function(frm, cdt, cdn){
+		let row = locals[cdt][cdn];
+		frappe.model.set_value(cdt, cdn, "cost", row.conversion_factor * row.last_purchase_rate);
+	}
+});
+frappe.ui.form.on('Contractors table', {
+	contractors_table_add: function(frm, cdt, cdn) {
+		let row = locals[cdt][cdn];
+		row.qty = 1;
+		row.cost = 0;
+		frm.refresh_field("contractors_table")
+	},
+	qty: function(frm, cdt, cdn){
+		let row = locals[cdt][cdn];
+		frappe.model.set_value(cdt, cdn, "total_cost", row.qty * row.cost);
+	},
+	cost: function(frm, cdt, cdn){
+		let row = locals[cdt][cdn];
+		frappe.model.set_value(cdt, cdn, "total_cost", row.qty * row.cost);
+	},
+	item: function(frm, cdt, cdn){
+		let row = locals[cdt][cdn];
+		frappe.db.get_value("Item", {"item_code": row.item}, "stock_uom", (r) => {
+			if (r) {
+				frappe.model.set_value(cdt, cdn, "uom", r.stock_uom);
+				frappe.model.set_value(cdt, cdn, "conversion_factor", 1);
+			}
+		})
+	},
+	uom: function(frm, cdt, cdn){
+		let row = locals[cdt][cdn];
+		set_conversion_factor(frm, row);
+	},
+	last_purchase_rate: function(frm, cdt, cdn){
+		let row = locals[cdt][cdn];
+		frappe.model.set_value(cdt, cdn, "cost", row.last_purchase_rate);
+	},
+	conversion_factor: function(frm, cdt, cdn){
+		let row = locals[cdt][cdn];
+		frappe.model.set_value(cdt, cdn, "cost", row.conversion_factor * row.last_purchase_rate);
+	}
+});
+
+frappe.ui.form.on('Expenses Table', {
+	expenses_table_add: function(frm, cdt, cdn) {
+		let row = locals[cdt][cdn];
+		row.qty = 1;
+		row.cost = 0;
+		frm.refresh_field("expenses_table")
+	},
+	qty: function(frm, cdt, cdn){
+		let row = locals[cdt][cdn];
+		frappe.model.set_value(cdt, cdn, "total_cost", row.qty * row.cost);
+	},
+	cost: function(frm, cdt, cdn){
+		let row = locals[cdt][cdn];
+		frappe.model.set_value(cdt, cdn, "total_cost", row.qty * row.cost);
+	},
+	item: function(frm, cdt, cdn){
+		let row = locals[cdt][cdn];
+		frappe.db.get_value("Item", {"item_code": row.item}, "stock_uom", (r) => {
+			if (r) {
+				frappe.model.set_value(cdt, cdn, "uom", r.stock_uom);
+				frappe.model.set_value(cdt, cdn, "conversion_factor", 1);
+			}
+		})
+	},
+	uom: function(frm, cdt, cdn){
+		let row = locals[cdt][cdn];
+		set_conversion_factor(frm, row);
+	},
+	last_purchase_rate: function(frm, cdt, cdn){
+		let row = locals[cdt][cdn];
+		frappe.model.set_value(cdt, cdn, "cost", row.last_purchase_rate);
+	},
+	conversion_factor: function(frm, cdt, cdn){
+		let row = locals[cdt][cdn];
+		frappe.model.set_value(cdt, cdn, "cost", row.conversion_factor * row.last_purchase_rate);
+	}
+});
+
+const set_conversion_factor = function(frm, row){
+	if (!row.item || !row.uom) return
+	frappe.call({
+		method: "erpnext.stock.get_item_details.get_conversion_factor",
+		args: {
+			"item_code": row.item,
+			"uom": row.uom
+		},
+		callback: function(r){
+			if (r.message){
+				frappe.model.set_value(row.doctype, row.name, "conversion_factor", r.message.conversion_factor);
+			}
+		}
+	})
+}
---
+++ furniture/furniture/doctype/boq/boq.py
@@ -0,0 +1,86 @@
+# Copyright (c) 2024, Jenan Alfahham and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+class BOQ(Document):
+	def validate(self):
+		tables = ["material_costs", "labor_costs", "contractors_table", "expenses_table"]
+
+		for table in tables:
+			total = 0
+			for item in self.get(table):
+				total += item.get("cost") * item.qty
+
+			if table == "contractors_table":
+				self.total_contractors = total
+			elif table == "expenses_table":
+				self.total_expenses = total
+			elif table == "material_costs":
+				self.total_material_costs = total
+			else:
+				self.total_labor_costs = total
+
+		
+		self.total_cost = self.get("total_material_costs", 0) + self.get("total_labor_costs", 0) + self.get("total_contractors", 0) + self.get("total_expenses", 0)
+
+	def on_submit(self):
+		if not self.costing_note or not self.line_id:
+			frappe.throw("The BOQ has no Costing Note as a Reference")
+
+		doc = frappe.get_doc("Costing Note", self.costing_note)
+		for row in doc.costing_note_items:
+			if row.name == self.line_id:
+				row.item = self.item
+				row.cost = self.total_cost 
+				row.total_cost = self.total_cost * self.project_qty
+				row.boq_link = self.name
+				for field in ["picture","body","skirt", "modesty", "metal", "color_gfim", "skirt_color", "modesty_color", "metal_color", "body_edge", "skirt_edge", "note", "width", "hight", "depth", "eng_description", "arabic_description"]:
+					setattr(row, field, self.get(field))
+				break
+
+		doc.save(ignore_permissions = True)
+
+	def on_cancel(self):
+		doc = frappe.get_doc("Costing Note", self.costing_note)
+		for row in doc.costing_note_items:
+			if row.name == self.line_id:
+				row.cost = 0
+				row.total_cost = 0
+				row.target_selling_price = 0
+				row.boq_link = None
+
+				break
+
+		doc.save(ignore_permissions = True)
+
+
+@frappe.whitelist()
+def set_boq_template(boq_template):
+
+	if not frappe.db.exists("Template BOQ", boq_template): return
+
+	tables = {}
+
+	temp = frappe.get_doc("Template BOQ", boq_template)
+
+	for table in ["material_costs", "labor_costs", "contractors_table", "expenses_table"]:
+		if temp.get(table):
+			
+			tables[table] = []
+
+			for row in temp.get(table):
+				item = frappe._dict({
+					"cost": row.get("cost", 0),
+					"qty": row.get("qty", 0), 
+					"item": row.item,
+					"total_cost": row.get("total_cost", 0)
+				})
+				if table == "material_costs": item["depreciasion_percentage"] = row.get("depreciasion_percentage")
+				else: item["uom"] = row.uom
+
+				tables[table].append(item)
+
+	return tables
+
